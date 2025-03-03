50865985e3184685f68d79836524c338b22fb42a
OakarOo <130909466+Mykal-Steele@users.noreply.github.com>
2025-03-03 22:30:10 +0700

    w

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// backend/index.js
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -6,12 +7,14 @@ import path from "path";
 import { fileURLToPath } from "url";
 import cloudinary from "cloudinary";
 import rateLimit from "express-rate-limit";
+import serverless from "serverless-http";
 
 dotenv.config();
 
 const app = express();
 
 const allowedOrigins = [
+  "https://www.adorio.space",
   "http://localhost:5173",
   "http://localhost:5174",
   "https://mykal-steele.github.io",
@@ -30,8 +33,8 @@ app.use(
 app.use(express.json());
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
 
@@ -47,10 +50,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/dist/index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html")); // Fixed path format
 });
 
 app.get("/", (req, res) => {
@@ -62,6 +65,9 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
+
+const handler = serverless(app);
+export { handler };
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
