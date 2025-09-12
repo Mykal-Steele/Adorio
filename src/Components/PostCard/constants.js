e236c6af605c4958a00c699ad99d03f4b031fb18
Mykal-Steele <mykal.stele@gmail.com>
2025-09-12 11:32:49 +0700

    idk

--- src/Components/PostCard/constants.js
+++ src/Components/PostCard/constants.js
@@ -1,5 +1,5 @@
 export const ADMIN_AVATAR_URL =
-  "https://res.cloudinary.com/dboeqtx65/image/upload/v1741248451/feelio/posts/file.jpg";
+  'https://res.cloudinary.com/dboeqtx65/image/upload/v1757651555/feelio/posts/file.jpg?postId=68c3a264395205e3cd502999&instance=post-68c3a264395205e3cd502999-1757651555819-gh819r&t=1757651555821&r=l1pk7knhr58';
 export const MAX_PREVIEW_LENGTH = 150;
 
-export const isUserAdmin = (username) => username === "Admin";
+export const isUserAdmin = (username) => username === 'Admin';
