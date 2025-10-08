b94f466465f92fc79dda45664792a3d64a85eb53
Mykal-Steele <mykal.stele@gmail.com>
2025-10-08 09:21:55 +0700

    not sure what i did

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-// main entry point - nothing fancy here
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import "./index.css";
-import "./App.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import './index.css';
+import './App.css';
 
-import App from "./App";
+import App from './App';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
