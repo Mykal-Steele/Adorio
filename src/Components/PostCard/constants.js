95878729d43b3ba039294661b4116258240aafcd
Mykal-Steele <mykal.stele@gmail.com>
2025-12-11 15:19:03 +0700

    ah finally admin profile

--- src/Components/PostCard/constants.js
+++ src/Components/PostCard/constants.js
@@ -1,4 +1,5 @@
-export const ADMIN_AVATAR_URL = '';
+export const ADMIN_AVATAR_URL =
+  "https://res.cloudinary.com/dboeqtx65/image/upload/c_limit,h_1200,w_1200/f_auto/q_auto:good/v1765217561/feelio/posts/file_kwzrfk?postId=6937151ab0c14b5dfcc076f7&instance=post-6937151ab0c14b5dfcc076f7-1765440973680-os69pn&t=1765440973684&r=8h1i1r5sx68";
 export const MAX_PREVIEW_LENGTH = 150;
 
-export const isUserAdmin = (username) => username === 'Admin';
+export const isUserAdmin = (username) => username === "Admin";
